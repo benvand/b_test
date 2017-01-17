3d0415461233f49f243bc6d9003738bbdf4fbcc4
benvand <benjaminfvandersteen@gmail.com>
2017-01-17 18:57:15 +0000

    foo

--- setup.py
+++ setup.py
@@ -5,10 +5,16 @@ import pip.download
 from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
+
 version='1'
 
-requirements = list(parse_requirements('requirements.txt',
-                                       session=pip.download.PipSession()))
+requirements = list(parse_requirements(
+    'requirements.txt',
+    session=pip.download.PipSession()
+))
+
+install_requires = [str(r.req) for r in requirements]
+
 
 setup(
     name='test_b',
@@ -20,6 +26,5 @@ setup(
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['git+https://github.com/benvand/c_test.git'],
-    dependency_links=['git+https://github.com/benvand/c_test.git'],
+    install_requires=install_requires,
 )
